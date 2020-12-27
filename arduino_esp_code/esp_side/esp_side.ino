23fcc72ef3e3a29bb1c0d950930b3776cf7f2cfa
bgsw0709 <57007068+bgsw0709@users.noreply.github.com>
2020-12-27 17:28:23 +0800

    update komen di code arduino dikit

--- arduino_esp_code/esp_side/esp_side.ino
+++ arduino_esp_code/esp_side/esp_side.ino
@@ -11,15 +11,15 @@ int wifiLastStatus;
  * GPIO2 -> D12 (kalo yang ini ga perlu, soalnya ini ngirim signal ke arduino nya)
  * vcc -> sumber 3.3v
  * gnd -> gnd (inget buatin common ground, klo keduanya make supply yg beda)
- * 
- * 
- */ 
+ *
+ *
+ */
 
 void setup() {
-  Serial.begin(115200);            
-  WiFi.begin("Laravel 2", "pENgUKuh@PeGUyanGAn?!421060");   
+  Serial.begin(115200);
+  WiFi.begin("Laravel 2", "pENgUKuh@PeGUyanGAn?!421060");
   pinMode(2, OUTPUT); // set gpio2 output untuk ngasi tau state garasi di server ke arduino
-  pinMode(0, INPUT); // set gpio0 output untuk ngasi tau state garasi di server ke arduino
+  pinMode(0, INPUT); // set gpio0 input untuk nge baca state garasi di local nya (yang dari arduino)
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Waiting for connection");
@@ -29,37 +29,37 @@ void setup() {
     }
   }
 }
- 
+
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) { 
+  if (WiFi.status() == WL_CONNECTED) {
     if (wifiLastStatus != 0) { // kondisi untuk semisal koneksinya putus, utk mastiin server sama local nya sync
         wifiLastStatus = 0;
           if (gateLastState == 0) { //yg di cek last state nya, klo last nya close, di server post untuk close, sebaliknya buat yg open jg
             HTTPClient http;
-            http.begin("http://iot.alindeveloper.xyz/api/closegate");      
-            http.POST("");  
+            http.begin("http://iot.alindeveloper.xyz/api/closegate");
+            http.POST("");
             http.end();
           }
           else {
-            HTTPClient http;   
-            http.begin("http://iot.alindeveloper.xyz/api/opengate");      
-            http.POST("");  
+            HTTPClient http;
+            http.begin("http://iot.alindeveloper.xyz/api/opengate");
+            http.POST("");
             http.end();
           }
         }
-     
-     HTTPClient http;   
+
+     HTTPClient http;
      http.begin("http://iot.alindeveloper.xyz/api/readgate"); // baca state garasi di server
-     int httpCode = http.POST("");  
+     int httpCode = http.POST("");
      String payload = http.getString();
 
     if (httpCode == 200) { // pastiin respon nya 200
       StaticJsonBuffer<200> jsonBuffer;
       JsonObject& root = jsonBuffer.parseObject(payload); // parse body dari hasil post readgate di servernya
       gateState = root["state_right_now"];
-     
+
       Serial.print("http response: ");
-      Serial.println(httpCode);  
+      Serial.println(httpCode);
       Serial.print("gate state from server: ");
       Serial.println(gateState);
 
@@ -77,8 +77,8 @@ void loop() {
       Serial.println(httpCode);
     }
     http.end();
-  } 
-  
+  }
+
   else {
     wifiLastStatus = 1; //semisal wifinya dc, set ini jadi 1, biar bisa masuk ke yg paling pertama itu, mastiin kondisi di server sync saama yg di local
     Serial.println("Error in WiFi connection");
@@ -86,15 +86,15 @@ void loop() {
 
   Serial.print("Button pressed: ");
   Serial.println(digitalRead(0)); // nge read state pin D3 di arduino, tujuannya biar esp nya juga aware sama kondisi garasi di localnya
-   if (digitalRead(0) == HIGH) { 
+   if (digitalRead(0) == HIGH) {
     if (gateState == 1 || gateLastState == 1){ // make 2 gini tujuannya klo cmn gateState yg di pake kan misal putus ini ga akan berubah, jadi last nya di pake jg
       if (WiFi.status() != WL_CONNECTED) {
         digitalWrite(2, LOW); // klo misal ga ada koneksi, ubah state gpio2 nya, misal sebelumnya 1 (HIGH atau open), sekarang ubah jadi 0 (LOW atau close)
       }
       else { // klo ada koneksi, ubah state di servernya jg, klo yg ini kan dari open -> close, jadi close gate nya
-        HTTPClient http; 
-        http.begin("http://iot.alindeveloper.xyz/api/closegate");      
-        http.POST("");  
+        HTTPClient http;
+        http.begin("http://iot.alindeveloper.xyz/api/closegate");
+        http.POST("");
         http.end();
       }
       gateLastState = 0; // set last state nya jadi close
@@ -105,9 +105,9 @@ void loop() {
         digitalWrite(2, HIGH);
       }
       else {
-        HTTPClient http;   
-        http.begin("http://iot.alindeveloper.xyz/api/opengate");      
-        http.POST("");  
+        HTTPClient http;
+        http.begin("http://iot.alindeveloper.xyz/api/opengate");
+        http.POST("");
         http.end();
       }
       gateLastState = 1;
@@ -118,5 +118,5 @@ void loop() {
   Serial.print("Gate last state: ");
   Serial.println(gateLastState);
   Serial.println("=========================");
-  delay(2000);  
+  delay(2000);
 }
